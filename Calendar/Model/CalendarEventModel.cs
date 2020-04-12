22ff10c2afb8d346ba3ba8d4dc05522a7503fbd6
Maria Pia Sanchez <mapisanchezg@gmail.com>
2020-04-12 01:33:51 -0400

    fix(model): model fixed

---
+++ Calendar/Model/CalendarEventModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Model
+{
+	public class CalendarEventModel { }
+
+	public class CalendarEvent : INotifyPropertyChanged
+	{
+		private DateTime startDate;
+		private DateTime finishDate;
+		private string name;
+
+		public DateTime StartDate
+		{
+			get
+			{
+				return startDate;
+			}
+			set
+			{
+				if(startDate != value)
+				{
+					startDate = value;
+					RaiseDateTimePropertyChanged(Constants.StartDateEventProperty);
+				}
+			}
+		}
+
+		public DateTime FinishDate
+		{
+			get
+			{
+				return finishDate;
+			}
+			set
+			{
+				if (finishDate != value)
+				{
+					finishDate = value;
+					RaiseDateTimePropertyChanged(Constants.FinishDateEventProperty);
+				}
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				if (name != value)
+				{
+					name = value;
+					RaiseDateTimePropertyChanged(Constants.NameEventProperty);
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void RaiseDateTimePropertyChanged(string property)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+		}
+	}
+
+}
---
+++ Calendar/Model/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.Model
+{
+	static class Constants
+	{
+		public const string StartDateEventProperty = "StartDate";
+		public const string FinishDateEventProperty = "FinishDate";
+		public const string NameEventProperty = "Name";
+	}
+}
